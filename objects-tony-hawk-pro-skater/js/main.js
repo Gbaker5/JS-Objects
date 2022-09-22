2a50f59aea18496b274c1ae8a321c2f666241ac5
Gjarred Baker <gjarred23@gmail.com>
2022-09-22 08:05:10 -0500

    tony hawk

---
+++ objects-tony-hawk-pro-skater/js/main.js
@@ -0,0 +1,20 @@
+//Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+function TonyHawkCharacter(chName,chStance,chSponsor,chMove){
+this.characterName = chName
+this.stance = chStance
+this.sponsor = chSponsor
+this.specialMove = chMove
+this.flip = function(){
+    console.log('Kickflip!!!')
+}
+this.taunt = function(){
+    console.log('Dont Make me Use my ${this.specialMove}')
+}
+this.grab = function(){
+    console.log('MELON GRAB')
+}
+
+}
+let kareemCampbell = new TonyHawkCharacter('kareem','goofy','element','ghetto bird')
+
+TonyHawkCharacter()
